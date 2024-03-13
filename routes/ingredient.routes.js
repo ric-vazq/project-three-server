63a94b50c00382ecab6da4ab848bb9d26eb53c8e
Ricardo Vazquez <rickyrabv@gmail.com>
2024-03-13 18:29:35 +0100

    wrong branch but ingredient routes complete

--- routes/ingredient.routes.js
+++ routes/ingredient.routes.js
@@ -1,10 +1,18 @@
 const Ingredient = require("../models/Ingredient.model");
-const fileUploader = require("../config/cloudinary.config")
+const fileUploader = require("../config/cloudinary.config");
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/all-ingredients", async (req, res, next) => {});
+router.get("/all-ingredients", async (req, res, next) => {
+  try {
+    const ingredients = await Ingredient.find();
+    console.log(ingredients);
+    return res.status(200).json({ ingredients: ingredients });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post(
   "/image-upload",
@@ -21,12 +29,43 @@ router.post(
 // Create Ingredient
 router.post("/create-ingredient", async (req, res, next) => {
   try {
-    console.log(req.body);
+    const { name, calories, proteins, fats, carbs, imageUrl } = req.body;
+    // console.log(req.body);
+    if (name === "" || imageUrl === "") {
+      res.status(400).json({ message: "Name and Image are required fields!" });
+    }
     const createdIngredient = await Ingredient.create(req.body);
     res.status(200).json(createdIngredient);
   } catch (error) {
     next(error);
   }
 });
+
+// Find Ingredient
+router.get("/:ingredientId", async (req, res, next) => {
+  try {
+    const { ingredientId } = req.params;
+    const foundIngredient = await Ingredient.findById(ingredientId);
+    if (!foundIngredient) {
+      return res.status(400).json({ message: "Ingredient not found"});
+    }
+    return res.status(200).json(foundIngredient);
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.put("/:ingredientId/edit", async (req, res, next) => {
+  try {
+    const { ingredientId } = req.params; 
+    const {name, calories, proteins, fats, carbs, imageUrl } = req.body; 
+
+    const updatedIngredient = await Ingredient.findByIdAndUpdate(ingredientId, req.body, {new: true});
+    console.log(updatedIngredient);
+    return res.status(200).json(updatedIngredient)
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
